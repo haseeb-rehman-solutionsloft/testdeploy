26a9cc7070f691b4645eb4dbdc1a5ee4283d3267
haseeb-rehman-solutionsloft <haseeb.rehman@siltuionsloft.com>
2024-11-11 19:03:57 +0500

    add new line

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>This is my new project for the test deploy</h1>
       <h2>This is test </h2>
-    
+      <h3>sucessfully deployed on github page</h3>
     </div>
   );
 }
